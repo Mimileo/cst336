c248f4bdb52c2793a95dd2d60c230926a82ceed4
Mimi Leon <mleon@csumb.edu>
2017-09-23 17:46:18 +0000

    Add lab3_group

---
+++ Practice/practice4/morearrays.php
@@ -0,0 +1,21 @@
+<?php
+session_start(); //starts or continues an existing session
+// session like global and associative array
+// $players = array("Juan" => 20 ); // not neccessary to declare array but good practice
+// $players["John"] = 40;
+// $_SESSION['Juan'] = 40;
+//echo $players["John"];
+echo "<br/>";
+echo $players["Juan"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+
+    </body>
+</html>
