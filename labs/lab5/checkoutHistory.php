ac5992ff0469ed1298c422b1e2504ce210b6f5bb
Mimi Leon <mleon@csumb.edu>
2017-10-16 07:01:54 +0000

    Update lab5

--- labs/lab5/checkoutHistory.php
+++ labs/lab5/checkoutHistory.php
@@ -22,7 +22,7 @@ function displayCheckoutHistory() {
         
         echo  "<span> <b>Name:</b> ". $record['firstName'] . " " . $record['lastName'] . " <b>userId:</b> " . $record['userId'] . " <b>deviceId:</b> " . $record['deviceId'] . " <b>checkoutId:</b> " . $record['checkoutId'].
         " <b>checkoutDate:</b> ". $record['checkoutDate'] . " <br/><b>dueDate:</b> " . $record['dueDate'] . " <b>returnDate:</b> " . $record['returnDate'] . " " . $record['checkoutBy'] 
-        . " <b>checkInBy:</b> " . $record['checkinBy'] . " <br/><b>deviceName:</b> " . $record['deviceName'] . " <b>deviceType:</b> " . $record['deviceType'] . " <b>price:</b> $" . $record['price'] . " <b>status:</b> " . $record['status'] . " <b>Email:</b> " . $record['email'] . "</span>";
+        . " <b>checkInBy:</b> " . $record['checkinBy'] . " <br/><b>deviceName:</b> " . $record['deviceName'] . " <b>deviceType:</b> " . $record['deviceType'] . " <b>price:</b> $" . $record['price'] . " <b>status:</b> " . $record['status'] . " <b>Email:</b> " . $record['email'] . " ".$record['universityId'] ." ".$record['gender'] ." ".$record['phone']." ".$record['role']." ".$record['deptId']."</span>";
         
     }
     
