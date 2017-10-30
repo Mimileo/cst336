8d4c82aff8533d5f7e1f78773f18b696581b33c8
Mimi Leon <mleon@csumb.edu>
2017-10-30 05:49:39 +0000

    Add lab6

--- dbConnection.php
+++ dbConnection.php
@@ -20,6 +20,7 @@ function getDatabaseConnection(){
     //creates db connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
+    
     //display errors when accessing tables
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
